8d97db51e38e0f896493a4d461c4739cbd38a5e7
arkarMannAungMMOCC <arkarmannaung.mmocc@gmail.com>
2023-05-08 22:01:13 +0630

    first

---
+++ src/main/java/com/hotel/booking/controller/HotelBookingController.java
@@ -0,0 +1,13 @@
+package com.hotel.booking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HotelBookingController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "screen/index";
+	}
+}
